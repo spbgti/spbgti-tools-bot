d7a8eb240034e8a475f4028b5b4cc095c73c8999
anamah <mmakhneva@icloud.com>
2016-07-15 16:16:02 +0300

    Fixed various issues and bugs.

--- spbgtitoolsbot/settings.py
+++ spbgtitoolsbot/settings.py
@@ -123,6 +123,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+"""STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'staticfiles'),
-)
+)"""
