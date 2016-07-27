c131a17d8ae8904564bf010e256b31f6c02b4be2
Dmitriy Boger <kotvberloge@gmail.com>
2016-07-28 01:19:15 +0300

    ST-22 Add webhook to bot

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 
+
 if __name__ == "__main__":
+    #from telegramBot import bot
+    #bot.start()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spbgtitoolsbot.settings")
 
     from django.core.management import execute_from_command_line
--- telegramBot/apps.py
+++ telegramBot/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+import os
 
 class TelegrambotConfig(AppConfig):
     name = 'telegramBot'
 
+    def ready(self):
+        from . import bot
+        bot.start()
