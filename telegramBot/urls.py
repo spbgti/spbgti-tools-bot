ea81a8155c63cb2fc3a8d54ce94813c017372fd0
anamah <mmakhneva@icloud.com>
2016-07-27 21:48:53 +0300

    Merge branch 'master' of /Users/AM/Desktop/project/spbgti-tools-bot with conflicts.

--- telegramBot/urls.py
+++ telegramBot/urls.py
@@ -2,20 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 ]
-
-
-
-
-#from .views import CommandReceiveView
-
-#urlpatterns = [
-#    url(r'^bot/(?P<bot_token>.+)/$', CommandReceiveView.as_view(), name='command'),
-#]
 
 
