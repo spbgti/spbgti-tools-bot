f6106ade1a3794272c0c4aa213e2a11b99654813
Dmitriy Boger <kotvberloge@gmail.com>
2016-07-28 18:59:51 +0300

    bug fix

--- telegramBot/urls.py
+++ telegramBot/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import url
 from . import views
 from . import bot
-from spbgtitoolsbot import settings
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<token>.+)/$', bot.webhook),
--- telegramBot/views.py
+++ telegramBot/views.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import bot
@@ -5,7 +8,3 @@ from . import bot
 def index(request):
     if bot.start() == True:
        return HttpResponse("Это работает!")
-
-
-
-
