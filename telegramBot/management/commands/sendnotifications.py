8fc0c02ee41f3b1fd5109c8f9aae33da069c9450
b0g3r <kotvberloge@gmail.com>
2017-03-05 14:25:38 +0300

    fix evening notifications

--- telegramBot/management/commands/sendnotifications.py
+++ telegramBot/management/commands/sendnotifications.py
@@ -4,9 +4,11 @@ from spbgtitoolsbot.settings import TOKEN
 from telegramBot.models import User
 from telegramBot.callbacks import DayScheduleCallback
 from datetime import date, timedelta
+import logging
 
 bot = Bot(TOKEN)
 
+logger = logging.getLogger("telegramBot")
 
 class Command(BaseCommand):
     help = 'Sends notifications'
@@ -16,15 +18,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, time, **options):
         if time in ('7:00', '7:30', '8:00'):
-            day = date.today()
-            if day.isoweekday() > 5:
-                return
+            day = date.today().isoweekday()
         elif time in ('20:00', '21:00', '22:00', '23:00'):
-            day = date.today()+timedelta(days=1)
-            if 4 < day.isoweekday() < 7:
-                return
+            day = date.today()+timedelta(days=1).isoweekday()
         else:
             raise CommandError('wrong notification time')
+        if day > 5:
+            logger.info("No exercises today")
+            return
         for user in User.objects.filter(notification_time=time):
             message = DayScheduleCallback.generate_message_for_day(user.group_number, day)
-            bot.sendMessage(chat_id=user.telegram_id, text=message)
+            if message.count('--') == 4:  # no one exercise in the day
+                logger.info("No exercises today for {}".format(user.telegram_id))
+            else:
+                bot.sendMessage(chat_id=user.telegram_id, text=message)
+                logger.info("Send notification {} to {}".format(time, user.telegram_id))
