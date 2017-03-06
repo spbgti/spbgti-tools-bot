7ec2dd29c6146de952ec50009f5ef81015c8abe5
b0g3r <kotvberloge@gmail.com>
2017-03-06 22:54:23 +0300

    bug fix 2.0

--- telegramBot/management/commands/sendnotifications.py
+++ telegramBot/management/commands/sendnotifications.py
@@ -19,19 +19,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, time, **options):
         if time in ('7:00', '7:30', '8:00'):
-            day = date.today().isoweekday()
+            day_date = date.today()
         elif time in ('20:00', '21:00', '22:00', '23:00'):
-            day = (date.today()+timedelta(days=1)).isoweekday()
+            day_date = date.today()+timedelta(days=1)
         else:
             raise CommandError('wrong notification time')
 
-        if day > 5:
+        if day_date.isoweekday() > 5:
             logger.info("No exercises today")
             return
         for user in User.objects.filter(notification_time=time):
-            message = DayScheduleCallback.generate_message_for_day(user.group_number, day)
+            message = DayScheduleCallback.generate_message_for_day(user.group_number, day_date)
             if message.count('--') == 4:  # no one exercise in the day
                 logger.info("No exercises today for {}".format(user.telegram_id))
             else:
-                bot.sendMessage(chat_id=user.telegram_id, text=message)
+                bot.sendMessage(chat_id=user.telegram_id, text=message, parse_mode='markdown')
                 logger.info("Send notification {} to {}".format(time, user.telegram_id))
